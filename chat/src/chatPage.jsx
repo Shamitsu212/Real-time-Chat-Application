02a4464e7023c04fb99a64be9688a24ffb44e0cf
Alexandr Dorozhko <darklin314@gmail.com>
2025-05-08 14:55:29 +0300

    feat: Добавлена схемантика

--- chat/src/chatPage.jsx
+++ chat/src/chatPage.jsx
@@ -8,20 +8,20 @@ function ChatPage ({messages, sender, senderChange, addToMessages, addNewMessage
             <div className={styles.nameInputContainer}>
                 <input className={styles.nameInput} onChange={senderChange} placeholder="Введите ваш ник"></input>
             </div>
-            <div className={styles.container}>
-                <div className={styles.chatContainer}>
+            <section className={styles.container}>
+                <section className={styles.chatContainer}>
                     {messages.map((msg) => (
-                    <div className={msg.sender === sender ? styles.messageYou : styles.message} key={msg.id}>
-                        <span className={msg.sender === sender ? styles.senderYou : styles.sender}>{msg.sender}</span>
-                        <span className={styles.messangetext}>{msg.text}</span>
-                    </div>
+                    <article className={msg.sender === sender ? styles.messageYou : styles.message} key={msg.id}>
+                        <p className={msg.sender === sender ? styles.senderYou : styles.sender}>{msg.sender}</p>
+                        <p className={styles.messangetext}>{msg.text}</p>
+                    </article>
                     ))}              
-                </div>
+                </section>
                 <div className={styles.sendMessage}>
                     <input className={styles.inputMessage} placeholder="Напишите сообщение" onChange={addToMessages} value={userMessage}></input>
                     <button className={styles.buttonMessage} onClick={addNewMessages}>Отправить</button>
                 </div>
-            </div>
+            </section>
         </>
     );
 }
